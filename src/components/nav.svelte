7ba7fba2cf7248b1736b48e2e85ec0887f3ca91b
AmadeusNoah <79079061+AmadeusNoah@users.noreply.github.com>
2022-06-15 08:30:41 +0200

    nav centreret

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -25,7 +25,7 @@
 </script>
 
 <nav class="w-screen drop-shadow-lg">
-  <div class="flex max-w-[1200px] justify-center m-auto">
+  <div class="flex max-w-[1200px] justify-center m-auto pt-3">
     <div class="h-full grid items-center  ">
       <a sveltekit:prefetch href="/">
         <img class="h-[70px]" src="hdk_logo_cannon.png" alt="logo orange" />
